75112cf031f0a8744d264dc98947c8b5b3ba847d
oluoko <brianotieno586@gmail.com>
2024-11-03 14:56:14 +0300

    Murmur Matrix: Authentication Setup

--- README.md
+++ README.md
@@ -20,6 +20,14 @@ Open [http://localhost:3000](http://localhost:3000) with your browser to see the
 
 You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
 
+Use this command to generate your own
+
+### NEXTAUTH_SECRET
+
+    ```bash
+    openssl rand -base64 32
+    ```
+
 This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
 
 ## Learn More
---
+++ app/lib/auth.ts
@@ -0,0 +1,17 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import type { NextAuthOptions } from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+const prisma = new PrismaClient();
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    }),
+  ],
+};
