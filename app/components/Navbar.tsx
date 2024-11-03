c1a297eaa882ea71ecf2e87f6be742e311b09f4e
oluoko <brianotieno586@gmail.com>
2024-11-03 15:06:29 +0300

    Murmur Matrix: Navbar Component

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <nav className="flex px-10 justify-between fixed top-0 left-0 w-full bg-white">
+      <h1>
+        MurMur<span> Matrix</span>
+      </h1>
+    </nav>
+  );
+};
+
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { NextAuthProvider } from "./components/Provider";
+import Navbar from "./components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <NextAuthProvider>
+          <Navbar />
           <main> {children}</main>
         </NextAuthProvider>
       </body>
