3dc59858ff71e778dcc6afa8545c25fd5791317c
oluoko <brianotieno586@gmail.com>
2024-11-03 20:19:29 +0300

    Murmur Matrix

--- app/(dashboard)/chat/page.tsx
+++ app/(dashboard)/chat/page.tsx
@@ -1,5 +1,15 @@
-const Chat = () => {
-  return <div>Welcome to the chat</div>;
-};
+import { authOptions } from "@/app/lib/auth";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default Chat;
+export default async function Chat() {
+  const session = await getServerSession(authOptions);
+
+  if (!session) {
+    redirect("/");
+  }
+
+  return (
+    <div className="h-screen bg-gray-200 flex flex-col">The Chat says Hi!</div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import Image from "next/image";
 import { MainGithubLogin, MainGoogleLogin } from "./components/Buttons";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions);
+
+  if (session) {
+    redirect("/chat");
+  }
   return (
     <div className="max-w-xl mx-auto border rounded-lg p-10 mt-32">
       <h1 className="text-lg md:text-2xl font-semibold text-center">
