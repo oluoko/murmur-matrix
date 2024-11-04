54d6f0b3d641980181b0ff20fac31811dbba9ab6
oluoko <brianotieno586@gmail.com>
2024-11-04 18:20:33 +0300

    Murmur Matrix: Models Update

---
+++ app/action.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { getServerSession } from "next-auth";
+import { prisma } from "./lib/db";
+import { authOptions } from "./lib/auth";
+
+export async function postData(formData: FormData) {
+  "use server";
+
+  const session = await getServerSession(authOptions);
+  const message = formData.get("message");
+
+  const data = await prisma.message.create({
+    data: {
+      message: message as string,
+      email: session?.user?.email,
+    },
+  });
+}
