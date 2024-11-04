fa128054fab9b88f1a788df91c0499741e768e32
oluoko <brianotieno586@gmail.com>
2024-11-05 01:17:58 +0300

    Murmur Matrix: Styling the chat screen

--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -17,13 +17,13 @@ interface iAppProps {
 const ChatComponent = ({ data }: iAppProps) => {
   const [messages, setMessages] = useState(data);
 
-  // function to get day of the week from date string
+  // Function to get day of the week from date string
   const getDay = (date: string) => {
     const newDate = new Date(date);
     return newDate.toLocaleString("en-US", { weekday: "long" });
   };
 
-  // function to get the time in the format HH:MM AM/PM from date string
+  // Function to get the time in the format HH:MM AM/PM from date string
   const getTime = (date: string) => {
     const newDate = new Date(date);
     return newDate.toLocaleString("en-US", {
@@ -36,31 +36,46 @@ const ChatComponent = ({ data }: iAppProps) => {
   return (
     <div className="p-2 flex-grow max-h-full overflow-y-auto py-32">
       <div className="flex flex-col gap-4">
-        {messages.map((message, index) => (
-          <div key={index} className="w-3/4 md:w-2/5">
-            <div className="flex items-center gap-2 md:gap-4">
-              <div className="w-1/5 flex flex-col items-center justify-center">
-                <Image
-                  src={message.User.image as string}
-                  alt="Profile image of user"
-                  className="size-6 md:size-12 object-cover rounded-lg mr-4 mb-2"
-                  width={50}
-                  height={50}
-                />{" "}
-                <p className="font-light text-xs text-gray-600">
-                  {message.User.name}
-                </p>
-              </div>
-              <div className=" w-4/5 rounded-lg bg-white  shadow-md self-start">
-                <div className="p-2 px-4">{message.message}</div>
-                <div className="w-full border-t  border-slate-400/30 flex justify-between p-2 px-4 font-light text-xs text-gray-600">
-                  <span>{getDay(message.createdAt)}</span>
-                  <span>{getTime(message.createdAt)}</span>
+        {messages.map((message, index) => {
+          // Determine if the previous message was sent by the same user
+          const isSameUserAsPrevious =
+            index > 0 && messages[index - 1].User.name === message.User.name;
+
+          return (
+            <div key={index} className="w-3/4 md:w-2/5">
+              <div className="flex items-center gap-2 md:gap-4">
+                {/* Only show the profile image if this message is not from the same user as the previous message */}
+                <div className="w-1/5 flex flex-col items-center justify-center">
+                  {!isSameUserAsPrevious && (
+                    <>
+                      <Image
+                        src={message.User.image as string}
+                        alt="Profile image of user"
+                        className="size-6 md:size-12 object-cover rounded-lg mr-4 mb-2"
+                        width={50}
+                        height={50}
+                      />
+                      <p className="font-light text-xs text-gray-600">
+                        {message.User.name}
+                      </p>
+                    </>
+                  )}
+                </div>
+                <div
+                  className={`w-${
+                    isSameUserAsPrevious ? "full" : "4/5"
+                  } rounded-lg bg-white shadow-md self-start`}
+                >
+                  <div className="p-2 px-4">{message.message}</div>
+                  <div className="w-full border-t border-slate-400/30 flex justify-between p-1 px-4 font-light text-xs text-gray-600">
+                    <span>{getDay(message.createdAt)}</span>
+                    <span>{getTime(message.createdAt)}</span>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
