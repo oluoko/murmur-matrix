f77389ac5ad5c6e5aea42cb63e3af8056bb3d62b
oluoko <brianotieno586@gmail.com>
2024-11-05 01:06:53 +0300

    Murmur Matrix: Styling the messages in chat component

--- app/(dashboard)/chat/page.tsx
+++ app/(dashboard)/chat/page.tsx
@@ -1,10 +1,35 @@
+import ChatComponent from "@/app/components/Chat";
 import Form from "@/app/components/Form";
 import { authOptions } from "@/app/lib/auth";
+import { prisma } from "@/app/lib/db";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
+async function getData() {
+  const data = await prisma.message.findMany({
+    select: {
+      message: true,
+      id: true,
+      createdAt: true,
+      User: {
+        select: {
+          name: true,
+          image: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: "asc",
+    },
+    take: 50,
+  });
+
+  return data;
+}
+
 export default async function Chat() {
   const session = await getServerSession(authOptions);
+  const data = await getData();
 
   if (!session) {
     redirect("/");
@@ -12,6 +37,7 @@ export default async function Chat() {
 
   return (
     <div className="h-[calc(100vh-120px)] bg-gray-200 flex flex-col relative overflow-hidden">
+      <ChatComponent data={data as any} />
       <Form />
     </div>
   );
--- app/components/Chat.tsx
+++ app/components/Chat.tsx
@@ -1,12 +1,67 @@
 "use client";
 
+import Image from "next/image";
 import { useState } from "react";
 
-const ChatComponent = () => {
-  const [messages, setMessages] = useState([]);
+interface iAppProps {
+  data: {
+    User: {
+      image: string | null;
+      name: string | null;
+    };
+    message: string;
+    createdAt: string;
+  }[];
+}
+
+const ChatComponent = ({ data }: iAppProps) => {
+  const [messages, setMessages] = useState(data);
+
+  // function to get day of the week from date string
+  const getDay = (date: string) => {
+    const newDate = new Date(date);
+    return newDate.toLocaleString("en-US", { weekday: "long" });
+  };
+
+  // function to get the time in the format HH:MM AM/PM from date string
+  const getTime = (date: string) => {
+    const newDate = new Date(date);
+    return newDate.toLocaleString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+  };
+
   return (
-    <div className="p-6 flex-grow max-h-full overflow-y-auto py-32">
-      <div className="flex flex-col gap-4"></div>
+    <div className="p-2 flex-grow max-h-full overflow-y-auto py-32">
+      <div className="flex flex-col gap-4">
+        {messages.map((message, index) => (
+          <div key={index} className="w-3/4 md:w-2/5">
+            <div className="flex items-center gap-2 md:gap-4">
+              <div className="w-1/5 flex flex-col items-center justify-center">
+                <Image
+                  src={message.User.image as string}
+                  alt="Profile image of user"
+                  className="size-6 md:size-12 object-cover rounded-lg mr-4 mb-2"
+                  width={50}
+                  height={50}
+                />{" "}
+                <p className="font-light text-xs text-gray-600">
+                  {message.User.name}
+                </p>
+              </div>
+              <div className=" w-4/5 rounded-lg bg-white  shadow-md self-start">
+                <div className="p-2 px-4">{message.message}</div>
+                <div className="w-full border-t  border-slate-400/30 flex justify-between p-2 px-4 font-light text-xs text-gray-600">
+                  <span>{getDay(message.createdAt)}</span>
+                  <span>{getTime(message.createdAt)}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
